a480f538b0874519feee0372d4855a513aa9f7a6
Dark2Knight7 <ducpeti27@gmail.com>
2022-05-20 13:03:28 +0200

    Hajo létrehozása

---
+++ TorpedoJatek/src/torpedojatek/Hajo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package torpedojatek;
+
+
+/**
+ *
+ * @author hallgato
+ */
+public class Hajo {
+    int[] pozicio = new int[3];
+    int talal;
+    
+    public Hajo(){
+        
+    }
+    
+    public String talalat(int poz){
+        return "Mellé";
+    }
+}
