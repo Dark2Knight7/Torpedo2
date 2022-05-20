7a6e860be40295547d094d167d57b529caafe976
Dark2Knight7 <ducpeti27@gmail.com>
2022-05-20 13:41:56 +0200

    Hajo class-ben scanner létrehozása

--- TorpedoJatek/src/torpedojatek/Hajo.java
+++ TorpedoJatek/src/torpedojatek/Hajo.java
@@ -1,19 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package torpedojatek;
 
 import java.util.Random;
+import java.util.Scanner;
 
-/**
- *
- * @author hallgato
- */
 public class Hajo {
     int[] pozicio = new int[3];
-    int talal;
+    int talalatSzam = 0;
     
     public Hajo(){
         setPozicio(pozicio);
@@ -22,6 +14,7 @@ public class Hajo {
     public void setPozicio(int[] pozicio){
         Random randomHely = new Random();
         int kezdoPoz = randomHely.nextInt(5);
+        assert kezdoPoz > 4: "Rossz helyen jött létre.";
         System.out.println("Kezdő elhelyezkedése a hajónak: "+kezdoPoz);
         for (int i = 0; i < pozicio.length; i++) {
             this.pozicio[i] = kezdoPoz;
@@ -30,12 +23,21 @@ public class Hajo {
     }
     
     public String talalat(int poz){
-        int i = 0;
-        while(i<pozicio.length && !(poz == pozicio[i])){
-            i++;
-        }
-        if(i<pozicio.length){
-            return "Talált";
+        System.out.println("Üdvözöllek a torpedó játékban.");
+        Scanner proba = new Scanner(System.in);
+        System.out.println("Csak 1-7-es pozíciókra tud támadni.");
+        System.out.println("Kérem adja meg milyen pozícióra szeretne támadni: ");
+        int lovesPoz = proba.nextInt()-1;
+        assert lovesPoz < 1: "1-esnél kisebb pozícióra nem lehet támadni!";
+        
+        while(talalatSzam < 3){
+            int i = 0;
+            while(i<pozicio.length && !(poz == pozicio[i])){
+                i++;
+            }
+            if(i<pozicio.length){
+                return "Talált";
+            }
         }
         return "Mellé";
     }
